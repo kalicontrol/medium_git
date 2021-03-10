2ec632893c3d9d3f0ce5f3fc5e72b90e0a270392
kalicontrol <seyitonbasi1889@hotmail.com>
2021-03-10 14:19:45 +0300

    carpma özelliği eklendi

--- topla.php
+++ topla.php
@@ -9,6 +9,18 @@
             $toplam += func_get_arg($i);
         }
         return $toplam;
+    
+    }
+    
+    function carp()
+    {
+        $carpim = 1;
+        $argumanSayisi = func_num_args();
+
+        for ($i = 0; $i < $argumanSayisi; $i++) {
+            $carpim *= func_get_arg($i);
+        }
+        return $carpim;
     }
 
 ?>
